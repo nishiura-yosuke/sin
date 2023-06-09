f5671c8e123587021a7f3a949e2ada02b75f7381
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-09 14:16:42 +0900

    gt

--- sinn.py
+++ sinn.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-if st.button('シャント動画'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+st.title("動画ボタン")
+
+if st.button("動画1"):
+        # ボタンが押された場合の処理
+        st.write("動画ボタンが押されました。")
+        # Googleのウェブサイトを開く
+        st.markdown("[Google](<https://www.google.com>)")
+
+
+
+
+if st.button("動画2"):
+        # ボタンが押された場合の処理
+        st.write("動画ボタンが押されました。")
+        # YouTubeの動画リンクを開く
+        st.markdown("[YouTube動画](<https://youtu.be/XbQ06MScArk>)")
+
