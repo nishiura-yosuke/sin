d0c720fa375e1c32c9cedc8ecf9bae9bcb59b217
nishiura yosuke <kenkenpa221202@gmail.com>
2023-06-12 14:21:09 +0900

    2kcolm

--- sinn.py
+++ sinn.py
@@ -2,23 +2,35 @@ import webbrowser
 import streamlit as st
 import pandas as pd
 
-
-
-
 # CSVファイルを読み込み、Pandas DataFrameに保存する
 data = pd.read_excel("logoform.xlsx")
 
 # Streamlitアプリを作成する
 def app():
+    # アプリのタイトルを表示
     st.title("各種申請")
+
+    # レイアウトを調整するためにカラムを使用する
+    col1, col2 = st.columns(2)
 
     # 動画リストを表示する
     for index, row in data.iterrows():
         title = row["タイトル"]
         url = row["URL"]
-        if st.button(title):
-            webbrowser.open_new_tab(url)
+
+        # 偶数番目の動画は左側のカラムに、奇数番目の動画は右側のカラムにボタンを配置する
+        if index % 2 == 0:
+            with col1:
+                if st.button(title):
+                    # ボタンがクリックされた場合、対応するURLを開く
+                    webbrowser.open_new_tab(url)
+        else:
+            with col2:
+                if st.button(title):
+                    # ボタンがクリックされた場合、対応するURLを開く
+                    webbrowser.open_new_tab(url)
 
 if __name__ == "__main__":
     app()
+
 
